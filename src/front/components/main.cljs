cebd25c542062fcd13724a83917d334fe534c67d
howard chang <idhowardgj94@gmail.com>
2022-05-11 01:06:55 +0800

    frontend boilerplate

--- src/front/components/main.cljs
+++ src/front/components/main.cljs
@@ -11,10 +11,10 @@
 (defc main-page < rum/reactive
   {:did-mount did-mount}
   [res]
-  [:div.lg:container.mx-auto.flex-row.flex
+  [:div.lg:container.mx-auto.flex-row.flex.full-height
    [:div.navigator 
     [:div 
-     [:input.search-bar.rounded.mx-auto.w-full.focus:outline-none.pl-2.mt-2.bg-gray-100 {:type "text" :placeholder "Search"}]]
+     [:input.search-bar.rounded.mx-auto.w-full.focus:outline-none.pl-2.mt-2.bg-gray-100 {:type "text" :placeholder "Search"} ]]
     [:ul.bg-white-200.overflow-hidden
      (friend-card)
      (friend-card)
--- src/front/components/room.cljs
+++ src/front/components/room.cljs
@@ -3,8 +3,24 @@
 
 (defc room < rum/reactive
   []
-  [:div.flex.flex-row.room-container
-   [:div.header.bg-yellow-100.flex.flex-1.items-center 
-    [:div.mx-4 
-     [:span
-      "家齊, 鬆恩, 立昕（3）" [:i.fa.fa-volume-up.fa-xl.ml-1 {:style {:color "grey"}}]]]]])
+  [:div.flex.flex-col.room-container
+   [:div.flex.flex-row
+    [:div.header.flex.flex-1.items-center
+     [:div.mx-4.flex.flex-1
+      [:div
+       "家齊, 鬆恩, 立昕（3）" [:i.fa.fa-volume-up.fa-xl.ml-1 {:style {:color "grey"}}]]
+      [:ul.to-right.flex.flex-row
+       [:li.ml-2 [:i.fa.fa-xl.fa-eye]]
+       [:li.ml-2 [:i.fa.fa-xl.fa-file]]
+       [:li.ml-2 [:i.fa.fa-bars.fa-xl]]]]]]
+   [:div.block.flex-1.message-container
+    [:div.flex.mb-2 [:span.px-4.rounded-xl.border-gray-700.border-opacity-100.border.ml-2 "hello, it's me"]]
+    [:div.flex.mb-2 [:span.px-4.rounded-xl.border-gray-700.border-opacity-100.border.ml-2 "hello, it's me"]]
+    [:div.flex.mb-2 [:span.px-4.rounded-xl.border-gray-700.border-opacity-100.border.ml-2 "hello, it's me"]]
+    [:div.flex.mb-2 [:span.px-4.rounded-xl.border-gray-700.border-opacity-100.border.mr-2.ml-auto "hello, it's me"]]
+    [:div.flex.mb-2 [:span.px-4.rounded-xl.border-gray-700.border-opacity-100.border.mr-2.ml-auto "hello, it's me"]]]
+   [:div.message-toolbox.flex.justify-center.items-center
+    [:div [:input {:type "text" :placeholder "input"}]
+     [:button.round.bg-blue-600.hover:bg-blue-400.p-2.rounded-xl.text-white.ml-4 "send"]]
+    ]]
+  )
