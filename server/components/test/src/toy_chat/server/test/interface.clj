79b4e2a5c9dc0515a86860c991b45b52b71c8ee7
howard chang <idhowardgj94@gmail.com>
2023-02-02 22:23:20 +0800

    init

---
+++ server/components/test/src/toy_chat/server/test/interface.clj
@@ -0,0 +1,1 @@
+(ns toy-chat.server.test.interface)
---
+++ server/components/test/test/toy_chat/server/test/interface_test.clj
@@ -0,0 +1,6 @@
+(ns toy-chat.server.test.interface-test
+  (:require [clojure.test :as test :refer :all]
+            [toy-chat.server.test.interface :as test]))
+
+(deftest dummy-test
+  (is (= 1 1)))
